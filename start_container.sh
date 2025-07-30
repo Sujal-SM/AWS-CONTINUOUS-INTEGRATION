4dbbb089ea3d98a8334bd24714d4b5fac38b2ad2
Sujal Malhotra <malhotrasujal98@gmail.com>
2025-07-30 18:29:16 +0530

    commited docker push

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull <docker-pull-command>
+docker pull trimankaur111905/simple-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 <docker-pull-command>
+docker run -d -p 5000:5000 trimankaur111905/simple-python-app:latest
